f0b020bbee7cad0cac332d5af532eaff49140258
Ebihara Yusuke <me@ebiyuu.com>
2021-12-22 19:21:55 +0900

    chore: setup img/scss loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,37 @@
-const path = require("path");
-const glob = require("glob");
-var fs = require("fs");
-// const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const glob = require('glob');
+var fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const webpackConfig = {
-  entry: { "common.js": "./src/common.js" },
+  entry: { 'common.js': './src/common.js' },
   output: {
-    filename: "[name]",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name]',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 20000,
+              name: '[name].[ext]',
+              esModule: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
@@ -23,26 +39,37 @@ const webpackConfig = {
 };
 
 // cf. https://www.key-p.com/blog/staff/archives/107125
-glob.sync("**/*(*.js|*.ts)", { cwd: "src" }).forEach((scriptName) => {
-  const scriptBaseName = path.basename(path.basename(scriptName, ".js"), ".ts");
-  const jsName = scriptBaseName + ".js";
-  const htmlName = scriptBaseName + ".html";
-  const htmlPath = path.resolve(__dirname, "src", htmlName);
+glob.sync('**/*.html', { cwd: 'src' }).forEach((htmlName) => {
+  const baseName = path.basename(htmlName, '.html');
+  const jsName = baseName + '.js';
+  const jsPath = path.resolve('src', jsName);
+  const tsName = baseName + '.ts';
+  const tsPath = path.resolve('src', tsName);
+  const scssName = baseName + '.scss';
+  const scssPath = path.resolve('src', scssName);
 
-  // htmlが存在するときのみ処理を行う
-  if (fs.existsSync(htmlPath)) {
-    // js
-    webpackConfig.entry[jsName] = path.resolve("src", scriptName);
-    // html
-    webpackConfig.plugins.push(
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src", htmlName),
-        filename: htmlName,
-        inject: "body",
-        chunks: ["common.js", jsName],
-      })
-    );
+  let chunks = ['common.js'];
+  if (fs.existsSync(tsPath)) {
+    webpackConfig.entry[jsName] = tsPath;
+    chunks.push(jsName);
+  } else if (fs.existsSync(jsPath)) {
+    webpackConfig.entry[jsName] = jsPath;
+    chunks.push(jsName);
   }
+
+  if (fs.existsSync(scssPath)) {
+    webpackConfig.entry[scssName + '.js'] = scssPath;
+    chunks.push(scssName + '.js');
+  }
+
+  webpackConfig.plugins.push(
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', htmlName),
+      filename: htmlName,
+      inject: 'body',
+      chunks,
+    }),
+  );
 });
 
 module.exports = webpackConfig;
